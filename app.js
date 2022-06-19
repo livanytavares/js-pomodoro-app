bcfe98707b71501ca134e992b1e80195a20ab256
livanytavares <livanynunes@gmail.com>
2022-06-19 14:27:50 -0300

    Setup app to use Pug template engine

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const express = require("express");
+const express = require("express")
+
 const app = express()
 const port = 3000
 const path = require('path')
 
-app.use(express.static(__dirname + 'public'))
+app.use(express.static('public'))
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'public/views'))
+
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.render('index', {title: 'Pomodoro app'})
 })
+
 
 app.listen(port, ()=>{
     console.log(`App listening on port ${port}`)
 })
+
