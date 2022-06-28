d24714287bd6007bbafb73dc26d9d9353e5ee451
livanytavares <livanynunes@gmail.com>
2022-06-28 20:30:45 -0300

    refactor: extract countDown function

--- public/utils/pomodoro.js
+++ public/utils/pomodoro.js
@@ -7,32 +7,17 @@ const SHORT_BREAK_MINUTES = 5 * MINUTES
 const LONG_BREAK_MINUTES = 15 * MINUTES
 
 var timerId;
-var isTimerRunning = false;
 const pauseTimer = () => {return isTimerRunning = false}
 const startTimer = () => {return isTimerRunning = true}
 var countDownMinutes = 29
 var countDownSeconds = 60
+var isTimerRunning = false;
 
 const startPomodoro = () => {
     startTimer()
     
     if(!timerId){
-        timerId = setInterval(() => {
-            if(isTimerRunning){
-                if(countDownSeconds == 0 && countDownMinutes >= 0){
-                    countDownSeconds = 60
-                    countDownMinutes-- 
-                }
-                countDownSeconds--
-
-                if (countDownMinutes == 0 && countDownSeconds == 0) {
-                    //TODO: Render the message on the UI
-                    console.log('Time Up!')
-
-                }
-                document.querySelector('#countDown').innerHTML = `${countDownMinutes}:${countDownSeconds}`
-            }
-        }, 1000) 
+        timerId = countDown() 
     }
 }
 
@@ -43,9 +28,28 @@ const pausePomodoro = () => {
 const resetPomodoro = () => {
     clearInterval(timerId)
     timerId = null
-    countDownMinutes = 29
-    countDownSeconds = 60
+    countDownMinutes = 29 //?
+    countDownSeconds = 60//?
     document.querySelector('#countDown').innerHTML = `0`
+}
+
+const countDown = () => {
+    return setInterval(() => {
+    
+        if(isTimerRunning){
+            if(countDownSeconds == 0 && countDownMinutes >= 0){
+                countDownSeconds = 60
+                countDownMinutes-- 
+            }
+            countDownSeconds--
+    
+            if (countDownMinutes == 0 && countDownSeconds == 0) {
+                //TODO: Render the message on the UI
+                console.log('Time Up!')
+    
+            }
+            document.querySelector('#countDown').innerHTML = `${countDownMinutes}:${countDownSeconds}`
+        }}, 1000)
 }
 
 
