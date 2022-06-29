a9756b06c8d34b79cdf41fef1881a2c080e9bf7f
livanytavares <livanynunes@gmail.com>
2022-06-28 21:04:16 -0300

    Refactor: create a Timer class

--- public/utils/pomodoro.js
+++ public/utils/pomodoro.js
@@ -6,50 +6,68 @@ const POMODORO_MINUTES = 30 * MINUTES
 const SHORT_BREAK_MINUTES = 5 * MINUTES
 const LONG_BREAK_MINUTES = 15 * MINUTES
 
-var timerId;
-const pauseTimer = () => {return isTimerRunning = false}
-const startTimer = () => {return isTimerRunning = true}
-var countDownMinutes = 29
-var countDownSeconds = 60
-var isTimerRunning = false;
+class Timer {
+    seconds = 60
+    isRunning = false
+    timerID = null
+
+    constructor(minutes){
+        this.minutes = minutes
+    }
+
+    countDown = () => {
+        return setInterval(() => {
+        
+            if(this.isRunning){
+                if(this.seconds == 0 && this.minutes >= 0){
+                    this.seconds = 60
+                    this.minutes-- 
+                }
+                this.seconds--
+        
+                if (this.minutes == 0 && this.seconds == 0) {
+                    //TODO: Render the message on the UI
+                    console.log('Time Up!')
+        
+                }
+                document.querySelector('#countDown').innerHTML = `${this.minutes}:${this.seconds}`
+            }}, 1000)
+    }
+
+    start = () => {
+        this.startTimer()
+        
+        if(!this.timerID){
+            this.timerID = this.countDown() 
+        }
+    }
+
+    pause = () => {
+        this.pauseTimer()
+    }
+
+    reset = () => {
+        clearInterval(this.timerID)
+        this.timerID = null
+        this.minutes = 29 //?
+        this.seconds = 60
+        document.querySelector('#countDown').innerHTML = `0`
+    }
+
+    startTimer = () => {return this.isRunning = true}
+    pauseTimer = () => {return this.isRunning = false}
+}
+
+const timer = new Timer(29)
 
 const startPomodoro = () => {
-    startTimer()
-    
-    if(!timerId){
-        timerId = countDown() 
-    }
+    timer.start()
 }
 
 const pausePomodoro = () => {
-    pauseTimer()
+    timer.pause()
 }
 
 const resetPomodoro = () => {
-    clearInterval(timerId)
-    timerId = null
-    countDownMinutes = 29 //?
-    countDownSeconds = 60//?
-    document.querySelector('#countDown').innerHTML = `0`
+    timer.reset()
 }
-
-const countDown = () => {
-    return setInterval(() => {
-    
-        if(isTimerRunning){
-            if(countDownSeconds == 0 && countDownMinutes >= 0){
-                countDownSeconds = 60
-                countDownMinutes-- 
-            }
-            countDownSeconds--
-    
-            if (countDownMinutes == 0 && countDownSeconds == 0) {
-                //TODO: Render the message on the UI
-                console.log('Time Up!')
-    
-            }
-            document.querySelector('#countDown').innerHTML = `${countDownMinutes}:${countDownSeconds}`
-        }}, 1000)
-}
-
-
